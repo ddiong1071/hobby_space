36de2e82f0bea99742cd8bcdd5841b0561e836dd
ddiong1071 <worm1071@nate.com>
2017-07-06 09:45:30 +0900

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app		= express();
 
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune.js');
 
 //핸들바 뷰 엔진설정 
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
@@ -23,8 +17,7 @@ app
 		res.render('home');
 	}) 
 	.get('/about', function(req, res){
-		var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-		res.render('about', { fortune : randomFortune });
+		res.render('about', { fortune : fortune.getFortune() });
 	});
 
 // 404 폴백 핸들러(미들웨어)
